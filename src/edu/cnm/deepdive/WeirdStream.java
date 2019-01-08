4fce08fbaebd6339ce5f6a5e3a3561ee956c4d63
samuelandazola <szola3@icloud.com>
2019-01-08 13:54:51 -0700

    Sorted overload with Comparator<Integer>.

--- src/edu/cnm/deepdive/WeirdStream.java
+++ src/edu/cnm/deepdive/WeirdStream.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive;
 
+import java.util.Comparator;
 import java.util.Random;
 import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
@@ -21,7 +22,12 @@ public class WeirdStream {
     })
         .limit(1000)
         .boxed()
-        .sorted()
+        .sorted(new Comparator<Integer>() {
+          @Override
+          public int compare(Integer o1, Integer o2) {
+            return o1.compareTo(o2);
+          }
+        })
         .forEach(System.out::println);
   }
 }
