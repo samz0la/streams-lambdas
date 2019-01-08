d90752174a42baa0e784529efafe3f51834919ae
samuelandazola <szola3@icloud.com>
2019-01-08 14:11:42 -0700

    Lambda for base-2 representation mapping.

--- src/edu/cnm/deepdive/WeirdStream.java
+++ src/edu/cnm/deepdive/WeirdStream.java
@@ -33,12 +33,7 @@ public class WeirdStream {
             return result;
           }
         })
-        .map(new Function<Integer, String>() {
-          @Override
-          public String apply(Integer integer) {
-            return Integer.toBinaryString(integer);
-          }
-        })
+        .map(value -> Integer.toBinaryString(value))
         .forEach(System.out::println);
   }
 }
